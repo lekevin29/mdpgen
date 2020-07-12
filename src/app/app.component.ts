c32eb715dc85ea3c06f9bc4bf479ad88da71632f
Kévin LE <lekevin29@gmail.com>
2020-07-12 08:37:59 +0200

    still no css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { splitAtColon } from '@angular/compiler/src/util';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,96 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'mdpgen';
+  longueur = 8;
+  mdp: string;
+  solidite: number;
+  phraseSolidite: string;
+  checkMajuscules = true;
+  checkMinuscules = true;
+  checkChiffres = true;
+  checkCaracteresSpeciaux = true;
+  checkMotDePasse = false;
+
+  generer(): void {
+    let result = '';
+    let caracteresSouhaites = '';
+    const majuscules = `ABCDEFGHIJKLMNOPQRSTUVWXYZ`;
+    const minuscules = `abcdefghijklmnopqrstuvwxyz`;
+    const chiffres = `01234567890123456789`;
+    const caracteresSpeciaux = `"'(-_çà)=~#{[|@]},;:!?./§<>$¤£µ*%ù`;
+    if (this.checkMajuscules === true) {
+      caracteresSouhaites += majuscules;
+    }
+    if (this.checkMinuscules === true) {
+      caracteresSouhaites += minuscules;
+    }
+    if (this.checkChiffres === true) {
+      caracteresSouhaites += chiffres;
+    }
+    if (this.checkCaracteresSpeciaux === true) {
+      caracteresSouhaites += caracteresSpeciaux;
+    }
+    const charactersLength = caracteresSouhaites.length;
+    for (let i = 0; i < this.longueur; i++) {
+      result += caracteresSouhaites.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    this.mdp = result;
+    this.checkMotDePasse = true;
+    this.calculSolidite();
+  }
+  plus(chiffre: number): void {
+    this.longueur += chiffre;
+    if (this.longueur > 32) {
+      this.longueur = 32;
+    }
+  }
+  moins(chiffre: number): void {
+    this.longueur -= chiffre;
+    if (this.longueur < 8) {
+      this.longueur = 8;
+    }
+  }
+  calculSolidite(): void {
+
+    let max = 0;
+    this.solidite = 0;
+
+    if (this.checkMajuscules) {
+      max += 20;
+      this.solidite += 10;
+    }
+    if (this.checkMinuscules) {
+      max += 20;
+      this.solidite += 10;
+    }
+    if (this.checkChiffres) {
+      max += 25;
+      this.solidite += 15;
+    }
+    if (this.checkCaracteresSpeciaux) {
+      max += 35;
+      this.solidite += 20;
+    }
+    this.solidite += 1.4 * this.longueur;
+
+    if (this.solidite > max) {
+      this.solidite = max;
+    }
+
+    this.solidite = Math.round(this.solidite);
+
+    if (this.solidite <= 25) {
+      this.phraseSolidite = 'ton mot de passe il est naze';
+    }
+    else if (this.solidite <= 50) {
+      this.phraseSolidite = 'ton mot de passe il est pas ouf';
+    }
+    else if (this.solidite <= 75) {
+      this.phraseSolidite = 'ton mot de passe il est bien ';
+    }
+    else if (this.solidite <= 100) {
+      this.phraseSolidite = 'ton mot de passe il est sécurisé de fou';
+    }
+  }
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ClipboardModule } from '@angular/cdk/clipboard';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 
 @NgModule({
   declarations: [
@@ -10,7 +15,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    FormsModule,
+    ClipboardModule,
+    MatProgressBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
